7e5b9052a0b197e7d7ef013f947ba9089741049d
sazol-sarker <sazolsarker1@gmail.com>
2023-04-01 18:27:35 +0600

    Scraped iplT20 auction table into csv file

--- iplT20_scraped.py
+++ iplT20_scraped.py
@@ -15,11 +15,32 @@ soup=BeautifulSoup(r.text,"lxml")
 table=soup.find("table",class_="ih-td-tab auction-tbl")
 
 #auction table extracted
-print(table)
+# print(table)
 
+#extracted table header
+header=table.find_all("th")
+# print(header)
 
+hTitle=[]
 
+for i in header:
+    title=i.text 
+    hTitle.append(title)
 
+# print(hTit le)
 
+df=pd.DataFrame(columns=hTitle)
+# print(df)
+ind =0
+rows=table.find_all("tr")
+for i in rows[1:]:
+    data=i.find_all("td")
+    # print(data)
+    row=[tr.text for tr in data]
+    df.loc[ind]=row
+    ind+=1
+    # print(row)
 
+print(df)
+df.to_csv("IPL_auction.csv")
 
