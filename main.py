5f4f38c64e1adda7399aa0f87ff2b240e58607ca
sazol-sarker <sazolsarker1@gmail.com>
2023-04-01 16:26:03 +0600

    added extract a target div content by id

--- main.py
+++ main.py
@@ -40,5 +40,47 @@ for links in anchors:
     if links!='#': 
          link="https://codewithharry.com"+links.get('href')
          all_links.add (link)  
-         print(link)
+        #  print(link)
 # print(all_links)
+
+#get all content of para from html
+html_comment="<p>A paragraph</p>"
+
+soup_comment=BeautifulSoup(html_comment)
+#content of paragraph
+# print(soup_comment.p)
+# # bs4.element.tag
+# print(type(soup_comment.p)) 
+
+
+#extracting content of a target div using its id
+
+
+
+url1="https://www.codewithharry.com/login/"
+
+
+r1= requests.get(url1)
+
+htmlContent1=r1.content
+# print(htmlContent)
+
+soup3=BeautifulSoup(htmlContent1,'html.parser')
+# print(soup3)
+
+imgpreview2=soup3.find(id="imgpreview2")
+# print(imgpreview2) # got the div
+# print(imgpreview2.contents) # got the div content source
+
+# for i in imgpreview2:
+#      print (i)# iterste childs
+
+
+
+#access login modal in soup
+mod=soup3.select('.Toastify')
+print(mod)
+
+
+
+
